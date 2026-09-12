d6293179779817047c7cb4fe2f2bccb01cbdac16
Mallow Horton <mallowhorton@mallowhorton.com>
2026-09-11 23:13:14 -0500

    Components work !

--- src/main/java/com/mallowwww/minecraft_simulated/mixin/SubLevelExtension.java
+++
@@ -1,9 +0,0 @@
-package com.mallowwww.minecraft_simulated.mixin;
-
-import com.mallowwww.minecraft_simulated.api.component.SubLevelComponent;
-
-public interface SubLevelExtension {
-    <T extends SubLevelComponent> boolean add(T component);
-    <T extends SubLevelComponent> boolean remove(T component);
-    int id();
-}
--- src/main/java/com/mallowwww/minecraft_simulated/registry/ModComponents.java
+++ src/main/java/com/mallowwww/minecraft_simulated/registry/ModComponents.java
@@ -1,5 +1,6 @@
 package com.mallowwww.minecraft_simulated.registry;
 
+import com.mallowwww.minecraft_simulated.MinecraftSimulated;
 import com.mallowwww.minecraft_simulated.api.SubLevelComponentRegistry;
 import com.mallowwww.minecraft_simulated.api.component.SubLevelComponent;
 import com.mallowwww.minecraft_simulated.api.component.SubLevelComponentType;
@@ -7,22 +8,27 @@ import com.mallowwww.minecraft_simulated.components.GoUpComponent;
 import dev.ryanhcode.sable.api.physics.handle.RigidBodyHandle;
 import dev.ryanhcode.sable.sublevel.ServerSubLevel;
 import net.minecraft.core.RegistryAccess;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.joml.Vector3d;
 
 public class ModComponents {
-    public static final DeferredRegister<SubLevelComponentType<?>> REGISTER = DeferredRegister.create(SubLevelComponentRegistry.COMPONENT_TYPES, "components");
+    public static final DeferredRegister<SubLevelComponentType<?>> REGISTER = DeferredRegister.create(SubLevelComponentRegistry.COMPONENT_TYPES, MinecraftSimulated.MOD_ID);
     public static final DeferredHolder<SubLevelComponentType<?>, SubLevelComponentType<GoUpComponent>> GO_UP_COMPONENT = REGISTER.register("go_up",
             (resourceLocation) -> new SubLevelComponentType.Builder<GoUpComponent>()
                     .tick(((subLevel, goUpComponent) -> {
                         // TODO makes it go up ig
                         if (subLevel instanceof ServerSubLevel serverSubLevel) {
                             var handle = RigidBodyHandle.of(serverSubLevel);
-                            handle.applyLinearImpulse(new Vector3d(0f, 3f, 0f));
+                            if (!handle.isValid()) return;
+                            handle.applyLinearImpulse(new Vector3d(0f, 0.5f, 0f));
                         }
                     }))
                     .create(GoUpComponent::new)
                     .build(resourceLocation)
     );
+    public static void register(IEventBus eventBus) {
+        REGISTER.register(eventBus);
+    }
 }
